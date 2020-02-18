4c903234e2631afe76eaa15ff8feca2d9925ec71
RazielO <oraziel_a@hotmail.com>
2020-02-17 20:35:44 -0600

    Regresar funciones a las originales
    Encadenamiento y equiparacion

--- Inferencia.hs
+++ Inferencia.hs
@@ -49,12 +49,13 @@ actualizar bh hecho = bh ++ [hecho]
 -- Si no, DEBERIA de preguntar
 equiparacion :: [[[[Char]]]] -> [[Char]] -> [Int]
 equiparacion bc bh
-    | length (masCorto (completas bc bh)) == 0 && not (elem cc (siguienteRegla bc bh)) = siguienteRegla bc bh
+    | length (masCorto (completas bc bh)) == 0 = siguienteRegla bc bh
     | otherwise = []
+--    | otherwise = pregunta bc bh
 
 -- Realiza el encadenamiento hacia adelante de forma recursiva
 encadenamiento bc bh i = do
-    let cc = equiparacion bc bh cc
+    let cc = equiparacion bc bh
     if i >= (length cc)
         then [bh, map show cc]
         else do
